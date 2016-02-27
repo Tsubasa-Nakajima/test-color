72c00d91e9308a036b83584349c107f27202d24d
Tsubasa NAKAJIMA <TSUBASA@solananos-macbook.local>
2016-02-27 18:47:44 +0900

    Initial Commit

---
+++ Test swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test swift
+//
+//  Created by Tsubasa NAKAJIMA on H28/02/27.
+//  Copyright © 平成28年 Tsubasa NAKAJIMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
