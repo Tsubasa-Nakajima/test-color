33064500e1859346be615ba316906f36fd37b7e3
Tsubasa NAKAJIMA <TSUBASA@solananos-MacBook.local>
2016-03-09 18:33:46 +0900

    sss

--- Test swift/ViewController.swift
+++ Test swift/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//    @IBOutlet weak var mySlider: UISlider!
+    
+    var ax = Int()
+    
+    @IBOutlet var label:UILabel!
+    @IBOutlet var label1:UILabel!
+    @IBOutlet var switchOnOff: UISwitch!
+    
+    @IBOutlet var sliderRed:UISlider!
+    @IBOutlet var sliderBlue:UISlider!
+    @IBOutlet var sliderGreen:UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        switchOnOff.on = false
+        label.text = "OFF"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func a() {
+        let redVal:CGFloat = CGFloat(sliderRed.value)
 
-
+        let greenVal:CGFloat = CGFloat(sliderGreen.value)
+  
+        let blueVal:CGFloat = CGFloat(sliderBlue.value)
+        
+        self.view.backgroundColor = UIColor(red: redVal, green: greenVal, blue: blueVal, alpha: 1.0)
+    }
+    
+    @IBAction func b() {
+        label.text = switchOnOff.on ? "ON" : "OFF"
+        if switchOnOff.on {
+            ax = ax + 1
+            label1.text = String(ax)
+        }
+    }
+    
 }
 
